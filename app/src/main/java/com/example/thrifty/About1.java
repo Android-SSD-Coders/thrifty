b6490036f55472577d37c69118ec479b9affac8f
saraalshater <gleave.alshater@gmail.com>
2021-12-01 12:54:20 +0200

    design1

---
+++ app/src/main/java/com/example/thrifty/About1.java
@@ -0,0 +1,30 @@
+package com.example.thrifty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toolbar;
+
+public class About1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about1);
+
+
+//        Button button1 = findViewById(R.id.Next1);
+//        button1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(About1.this,MainActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+   }
+
+}
---
+++ app/src/main/java/com/example/thrifty/About2.java
@@ -0,0 +1,25 @@
+package com.example.thrifty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class About2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about2);
+
+
+
+//        Button button2 = findViewById(R.id.Next2);
+//        button2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(About1.this,MainActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+    }
+}
