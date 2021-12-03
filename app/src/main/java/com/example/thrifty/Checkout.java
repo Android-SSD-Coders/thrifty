75336ab251286d3f85bfd92dcdbeb11af441814b
Heba1998 <hebaalmomani1998@gmail.com>
2021-12-04 00:50:28 +0200

    add toast

--- app/src/main/java/com/example/thrifty/Checkout.java
+++ app/src/main/java/com/example/thrifty/Checkout.java
@@ -3,6 +3,9 @@ package com.example.thrifty;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Checkout extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class Checkout extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
+
+
+        Button finalstep = findViewById(R.id.finalstep);
+        finalstep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "You checkout successfully, we will contact you later to tell you the state of your order", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
